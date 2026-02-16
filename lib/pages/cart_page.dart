8ce1befd09f68c6c53ac529eda2f5a0ad094d54c
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-02-16 21:47:51 +0300

    updated multi-page navigation

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_shop_app/model/card_model.dart';
 import 'package:provider/provider.dart';
 
@@ -8,37 +9,52 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Cart'), backgroundColor: Colors.blue, foregroundColor: Colors.white),
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0, iconTheme: IconThemeData(color: Colors.black)),
       body: Consumer<CardModel>(builder: (context, value, child) {
         return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "My Cart",
+                style: GoogleFonts.notoSerif(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
             //list of cart items
-            Expanded(child: ListView.builder(
-              itemCount: value.cartItems.length,
-              padding: EdgeInsets.all(12.0),
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: ListTile(
-                      leading: Image.asset(
-                        value.cartItems[index][2],
-                        height: 36,
+            Expanded(
+              child: value.cartItems.isEmpty
+                ? Center(child: Text("Your cart is empty!", style: GoogleFonts.notoSerif(fontSize: 18)))
+                : ListView.builder(
+                  itemCount: value.cartItems.length,
+                  padding: EdgeInsets.all(12.0),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index][2],
+                            height: 36,
+                          ),
+                          title: Text(value.cartItems[index][0]),
+                          subtitle: Text('\$' + value.cartItems[index][1]),
+                          trailing: IconButton(
+                            icon: Icon(Icons.cancel),
+                            onPressed: () => Provider.of<CardModel>(context, listen: false).removeItemFromCart(index),
+                          ),
+                        ),
                       ),
-                      title: Text(value.cartItems[index][0]),
-                      subtitle: Text('\$' + value.cartItems[index][1]),
-                      trailing: IconButton(
-                        icon: Icon(Icons.cancel),
-                        onPressed: () => Provider.of<CardModel>(context, listen: false).removeItemFromCart(index),
-                      ),
-                    ),
-                  ),
-                );
-            },)),
+                    );
+                },)
+            ),
 
             //total price + pay now
             Padding(
@@ -65,7 +81,7 @@ class CartPage extends StatelessWidget {
                       ],
                     ),
                     //pay now button
-                    Container(
+                    /*Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(12),
                         border: Border.all(color: Colors.green.shade100),
@@ -83,6 +99,29 @@ class CartPage extends StatelessWidget {
                             color: Colors.white,
                           )
                         ],
+                      ),
+                    ),*/
+                    GestureDetector(
+                      onTap: () {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => PaymentPage()));
+
+                        //error message
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Proceeding to payment...")),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.green.shade100),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        child: Row(
+                          children: const [
+                            Text("Pay Now", style: TextStyle(color: Colors.white)),
+                            Icon(Icons.arrow_forward_ios, size: 16, color: Colors.white),
+                          ],
+                        ),
                       ),
                     ),
                   ],

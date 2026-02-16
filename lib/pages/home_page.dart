7aa79bc50c14b76e5f0af6108a7fbdb269c63684
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-02-16 16:51:34 +0300

    google fonts and home_page file added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../components/grocery_item_tile.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 48),
+            //good morning
+            const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text("Good morning"),
+            ),
+
+            const SizedBox(height: 4,),
+
+            //Lets order fresh items for you
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                  "Let's order fresh items for you",
+                  style: GoogleFonts.notoSerif(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            ),
+
+            const SizedBox(height: 24),
+
+            //divider
+            const Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Divider(
+
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            //items + grid
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Fresh Items",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+
+            Expanded(
+              child: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return GroceryItemTile();
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home_page.dart';
 
 class IntroPage extends StatelessWidget{
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: Column(
         children: [
@@ -16,35 +17,44 @@ class IntroPage extends StatelessWidget{
           ),
 
           //delivering groceries
-          const Padding(
+          Padding(
             padding: const EdgeInsets.all(24.0),
             child: Text(
               "We deliver groceries at your doorstep",
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 40,
+              style: GoogleFonts.notoSerif(
+                fontSize: 36,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
 
+          const SizedBox(height: 24),
+
           //fresh items everyday
           Text(
             "Fresh items everyday",
+            style: TextStyle(color: Colors.grey[600]),
           ),
 
           const Spacer(),
 
           //get started button
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            padding: EdgeInsets.all(24),
-            child: Text(
-              "Get Started",
-              style: TextStyle(color: Colors.white),
+          GestureDetector(
+            onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+              return const HomePage();
+              },
+            )),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.all(24),
+              child: const Text(
+                "Get Started",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
 

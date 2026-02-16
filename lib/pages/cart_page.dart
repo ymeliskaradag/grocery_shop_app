5e933a43c684be16e7053b0a76f0e3b0c7e25e05
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-02-16 21:13:40 +0300

    refined card layout for better user experience

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -12,6 +12,7 @@ class CartPage extends StatelessWidget {
       body: Consumer<CardModel>(builder: (context, value, child) {
         return Column(
           children: [
+            //list of cart items
             Expanded(child: ListView.builder(
               itemCount: value.cartItems.length,
               padding: EdgeInsets.all(12.0),
@@ -37,7 +38,57 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                 );
-            },))
+            },)),
+
+            //total price + pay now
+            Padding(
+              padding: const EdgeInsetsGeometry.all(36.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.green, borderRadius: BorderRadius.circular(8)),
+                padding: EdgeInsets.all(24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //price
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Total Price",
+                          style: TextStyle(color: Colors.green[100]),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          '\$' + value.calculateTotal(),
+                          style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    //pay now button
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Colors.green.shade100),
+                      ),
+                      padding: EdgeInsets.all(12),
+                      child: Row(
+                        children: const [
+                          Text(
+                            "Pay Now",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         );
       }),

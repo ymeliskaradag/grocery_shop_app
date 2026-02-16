7f0d7ce8b3b007422609bce14cfb1e55d7d54b5d
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-02-16 18:44:46 +0300

    created reusable card component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import '../components/grocery_item_tile.dart';
+import '../model/card_model.dart';
+import 'card_page.dart';
 
 class HomePage extends StatelessWidget{
   const HomePage({super.key});
@@ -8,6 +11,13 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return CardPage();
+        })),
+        backgroundColor: Colors.black,
+        child: Icon(Icons.shopping_bag, color: Colors.white,),
+      ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -54,15 +64,22 @@ class HomePage extends StatelessWidget{
               ),
             ),
 
-            /*Expanded(
-              child: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                itemBuilder: (context, index) {
-                  return GroceryItemTile(
-
-                  );
-                },
-              ),
-            )*/
+            Expanded(
+              child: Consumer<CardModel>(builder: (context, value, child){
+                return GridView.builder(itemCount: value.shopItems.length,
+                  padding: EdgeInsets.all(12.0),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1 / 1.3,),
+                  itemBuilder: (context, index) {
+                    return GroceryItemTile(
+                      itemName: value.shopItems[index][0],
+                      itemPrice: value.shopItems[index][1],
+                      imagePath: value.shopItems[index][2],
+                      color: value.shopItems[index][3],
+                    );
+                  },
+                );
+              },),
+            )
           ],
         ),
       ),

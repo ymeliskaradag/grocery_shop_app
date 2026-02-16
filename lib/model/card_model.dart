0fa1fd29b3db7e5d56e52e74f9872ea3bbd108a2
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-02-16 19:16:28 +0300

    some methods created for shopping cart

--- lib/model/card_model.dart
+++ lib/model/card_model.dart
@@ -10,5 +10,31 @@ class CardModel extends ChangeNotifier{
     ["Water ", "1.00", "lib/images/water.png", Colors.blue],
   ];
 
+  //list of cart items
+  List _cartItems = [];
+
   get shopItems => _shopItems;
+
+  get cartItems => _cartItems;
+
+  //add item to cart
+  void addItemToCart(int index) {
+    _cartItems.add(_shopItems[index]);
+    notifyListeners();  //for updates
+  }
+
+  //remove item from cart
+  void removeItemFromCart(int index) {
+    _cartItems.removeAt(index);
+    notifyListeners();
+  }
+
+  //calculate total price
+  String calculateTotal() {
+    double totalPrice = 0;
+    for(int i = 0; i < _cartItems.length; i++){
+      totalPrice += double.parse(_cartItems[i][1]);
+    }
+    return totalPrice.toStringAsFixed(2);
+  }
 }
